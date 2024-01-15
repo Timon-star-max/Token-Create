bb6d65e6c6afbdf9d86fbc6ac3c29c168e732d6f
HirotoY107713 <hirotoyoki12345@gmail.com>
2024-01-15 10:19:39 +0800

    Input changes

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -21,23 +21,3 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
   },
 );
 Input.displayName = "Input";
-
-const InputCheckBox = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, checked, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className,
-        )}
-        ref={ref}
-        checked={checked}
-        {...props}
-      />
-    );
-  },
-);
-Input.displayName = "InputCheckBox";
-
-export { Input, InputCheckBox };
